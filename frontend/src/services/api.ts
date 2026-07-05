0766f03fd9402793282794a8fc2b283799a9c8e1
Naveen Reddy <kottenaveen558@gmail.com>
2026-07-05 21:53:40 +0530

    Prepare frontend for deployment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://codeguardian-ai-ye72.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
